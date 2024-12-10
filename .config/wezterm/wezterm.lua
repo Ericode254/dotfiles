c616dc0863582804e9e5111678b5cb1b440250b0
Ericode254 <jilloerick6@gmail.com>
2024-12-10 11:33:52 +0300

    cleaned my wezterm conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,11 +5,6 @@ require('utils.backdrops')
    -- :set_focus('#000000')
    :random()
 
-require('events.right-status').setup()
-require('events.left-status').setup()
-require('events.tab-title').setup()
-require('events.new-tab-button').setup()
-
 return Config:init()
    :append(require('config.appearance'))
    :append(require('config.bindings'))
